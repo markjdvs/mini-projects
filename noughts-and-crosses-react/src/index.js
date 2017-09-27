971f8bbc7b2cc8d01e579e3248ff3193588d78af
mrkjdvs <mjd814@gmail.com>
2017-09-27 18:42:59 +0100

    Status of game updated to declare if there is a winner or not.

--- noughts-and-crosses-react/src/index.js
+++ noughts-and-crosses-react/src/index.js
@@ -38,7 +38,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.state.xIsNext ? 'X' : '0';
+    }
 
     return (
       <div>
@@ -90,7 +96,13 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
