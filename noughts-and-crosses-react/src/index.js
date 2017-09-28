645645c3976b6de06f5710e6c203ef35662f2fbf
mrkjdvs <mjd814@gmail.com>
2017-09-28 19:34:57 +0100

    Mapping through history array of moves and injecting them as component.

--- noughts-and-crosses-react/src/index.js
+++ noughts-and-crosses-react/src/index.js
@@ -76,12 +76,22 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Move #' + move : 'Game Start';
+      return (
+        <li>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+
     return (
       <div className="game">
         <div className="game-board">
@@ -92,7 +102,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
