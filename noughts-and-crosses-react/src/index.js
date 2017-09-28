ffb877c86c525c5882abcc44bf34c6ae6e823961
mrkjdvs <mjd814@gmail.com>
2017-09-28 19:30:15 +0100

    Moved handleClick fn to Game class and adjusted.

--- noughts-and-crosses-react/src/index.js
+++ noughts-and-crosses-react/src/index.js
@@ -11,18 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if(calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
@@ -33,14 +21,6 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
-    }
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -73,6 +53,22 @@ class Game extends React.Component {
         }],
         xIsNext: true,
       }
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }])
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   render() {
